5250d19787b15ec551f115782b6fec70e09fcc5c
Yuriy <iandrus@exadel.com>
2018-10-03 09:45:18 +0300

    TS2

--- 2 lesson/TS_2_2.js
+++ 2 lesson/TS_2_2.js
@@ -11,11 +11,14 @@ class Garage {
     };
 }
 
-class Truck extends Garage{
+class Truck extends Garage {
     constructor(num, mark, wheelsnumber, numPlaces, diesel) {
-        super(num,wheelsnumber, numPlaces);
-        this.mark= mark;
+        super(num, wheelsnumber, numPlaces);
+        this.mark = mark;
+        diesel = true;
         this.diesel = diesel;
+        wheelsnumber = 8;
+        this.wheelsnumber = wheelsnumber;
     };
 
     introduce() {
@@ -25,6 +28,43 @@ class Truck extends Garage{
         else console.log("benzine engine.");
     };
 }
+
+class Auto extends Garage {
+    constructor(num, mark, wheelsnumber, numPlaces, diesel) {
+        super(num, wheelsnumber, numPlaces);
+        this.mark = mark;
+        diesel = false;
+        this.diesel = diesel;
+        wheelsnumber = 4;
+        this.wheelsnumber = wheelsnumber;
+    };
+
+    introduce() {
+        console.log("Hello, this is truck " + this.mark + " and ");
+        if (this.diesel)
+            console.log("diesel engine.");
+        else console.log("benzine engine.");
+    };
+}
+
+class Moto extends Garage {
+    constructor(num, mark, wheelsnumber, numPlaces, diesel) {
+        super(num, wheelsnumber, numPlaces);
+        this.mark = mark;
+        diesel = false;
+        this.diesel = diesel;
+        wheelsnumber = 2;
+        this.wheelsnumber = wheelsnumber;
+    };
+
+    introduce() {
+        console.log("Hello, this is truck " + this.mark + " and ");
+        if (this.diesel)
+            console.log("diesel engine.");
+        else console.log("benzine engine.");
+    };
+}
+
 //
 // Take as a basis an airport or a car park (or something that you want) and create
 // structure of classes using Prototypes and ES6 classes. Nesting should be at least 3 objects;
